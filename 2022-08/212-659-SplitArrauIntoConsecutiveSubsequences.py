bb655dffea2d6e571cf42be9562db84886a97732
kftlfd <kftlfd@ex.ua>
2022-08-19 12:01:25 +0300

    day 212

---
+++ 2022-08/212-659-SplitArrauIntoConsecutiveSubsequences.py
@@ -0,0 +1,73 @@
+"""
+Leetcode
+659. Split Array into Consecutive Subsequences (medium)
+2022-08-19
+
+You are given an integer array nums that is sorted in non-decreasing order.
+
+Determine if it is possible to split nums into one or more subsequences such that both of the following conditions are true:
+
+ - Each subsequence is a consecutive increasing sequence (i.e. each integer is exactly one more than the previous integer).
+ - All subsequences have a length of 3 or more.
+
+Return true if you can split nums according to the above conditions, or false otherwise.
+
+A subsequence of an array is a new array that is formed from the original array by deleting some (can be none) of the elements without disturbing the relative positions of the remaining elements. (i.e., [1,3,5] is a subsequence of [1,2,3,4,5] while [1,3,2] is not).
+
+Example 1:
+Input: nums = [1,2,3,3,4,5]
+Output: true
+Explanation: nums can be split into the following subsequences:
+[1,2,3,3,4,5] --> 1, 2, 3
+[1,2,3,3,4,5] --> 3, 4, 5
+
+Example 2:
+Input: nums = [1,2,3,3,4,4,5,5]
+Output: true
+Explanation: nums can be split into the following subsequences:
+[1,2,3,3,4,4,5,5] --> 1, 2, 3, 4, 5
+[1,2,3,3,4,4,5,5] --> 3, 4, 5
+
+Example 3:
+Input: nums = [1,2,3,4,4,5]
+Output: false
+Explanation: It is impossible to split nums into consecutive increasing subsequences of length 3 or more.
+"""
+
+from typing import List
+import collections
+
+
+# https://leetcode.com/problems/split-array-into-consecutive-subsequences/discuss/106514/C++Python-Esay-Understand-Solution
+# Runtime: 756 ms, faster than 61.12% of Python3 online submissions for Split Array into Consecutive Subsequences.
+# Memory Usage: 15.2 MB, less than 95.45% of Python3 online submissions for Split Array into Consecutive Subsequences.
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        left = collections.Counter(nums)
+        end = collections.Counter()
+        for i in nums:
+            if not left[i]:
+                continue
+            left[i] -= 1
+            if end[i - 1] > 0:
+                end[i - 1] -= 1
+                end[i] += 1
+            elif left[i + 1] and left[i + 2]:
+                left[i + 1] -= 1
+                left[i + 2] -= 1
+                end[i + 2] += 1
+            else:
+                return False
+        return True
+
+
+s = Solution()
+tests = [
+    [1, 2, 3, 3, 4, 5],
+    [1, 2, 3, 3, 4, 4, 5, 5],
+    [1, 2, 3, 4, 4, 5],
+]
+for t in tests:
+    print(t)
+    print(s.isPossible(t))
+    print()
