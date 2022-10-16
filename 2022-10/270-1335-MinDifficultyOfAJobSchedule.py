a1ce27adfa1beb41070ca70c8481c94a6fabcf40
kftlfd <kftlfd@ex.ua>
2022-10-16 21:51:29 +0300

    day 270

---
+++ 2022-10/270-1335-MinDifficultyOfAJobSchedule.py
@@ -0,0 +1,106 @@
+"""
+Leetcode
+1335. Minimum Difficulty of a Job Schedule (hard)
+2022-10-16
+
+You want to schedule a list of jobs in d days. Jobs are dependent (i.e To work on the ith job, you have to finish all the jobs j where 0 <= j < i).
+
+You have to finish at least one task every day. The difficulty of a job schedule is the sum of difficulties of each day of the d days. The difficulty of a day is the maximum difficulty of a job done on that day.
+
+You are given an integer array jobDifficulty and an integer d. The difficulty of the ith job is jobDifficulty[i].
+
+Return the minimum difficulty of a job schedule. If you cannot find a schedule for the jobs return -1.
+
+Example 1:
+Input: jobDifficulty = [6,5,4,3,2,1], d = 2
+Output: 7
+Explanation: First day you can finish the first 5 jobs, total difficulty = 6.
+Second day you can finish the last job, total difficulty = 1.
+The difficulty of the schedule = 6 + 1 = 7 
+
+Example 2:
+Input: jobDifficulty = [9,9,9], d = 4
+Output: -1
+Explanation: If you finish a job per day you will still have a free day. you cannot find a schedule for the given jobs.
+
+Example 3:
+Input: jobDifficulty = [1,1,1], d = 3
+Output: 3
+Explanation: The schedule is one job per day. total difficulty will be 3.
+"""
+
+from typing import List, Optional
+from functools import lru_cache
+
+
+# https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/discuss/2708161/LeetCode-The-Hard-Way-Explained-Line-By-Line
+# Runtime: 1188 ms, faster than 79.06% of Python3 online submissions for Minimum Difficulty of a Job Schedule.
+# Memory Usage: 14.8 MB, less than 57.75% of Python3 online submissions for Minimum Difficulty of a Job Schedule.
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+
+        if n < d:
+            return -1
+
+        @lru_cache(maxsize=None)
+        # dp(i, k): min difficulty when you start working on i-th job at day `k`
+        def dp(i, k):
+
+            # reach the last day
+            # we put all the remaining jobs on this day
+            # so we return the one with max difficulty
+            if k == d:
+                return max(jobDifficulty[i:])
+
+            # init min difficulty with inf
+            res = float('inf')
+
+            # cur is the max difficulty we've seen so far on day `k`
+            # init current max with 0
+            cur = 0
+
+            # for jobDifficulty like 6 5 4 3 2 1,
+            # we can have following ways to distribute them into two days
+            # 6 | 5 4 3 2 1
+            # 6 5 | 4 3 2 1
+            # 6 5 4 | 3 2 1
+            # 6 5 4 3 | 2 1
+            # 6 5 4 3 2 | 1
+            # notice that each day we must have at least one task
+            # given the starting index `i`,
+            # we can only at most choose the jobs till the position `n - d + k - 1`
+
+            for j in range(i, n - d + k):
+                cur = max(cur, jobDifficulty[j])
+                # if j-th job is the last job on day `k`,
+                # the max difficulty for day `k` is `cur`
+                # and we need to start (j + 1)-th job on the next day
+                # the result would be `cur + dp(j + 1, k + 1)`
+                # then we take the min
+                res = min(res, cur + dp(j + 1, k + 1))
+            return res
+
+        # we start working on 0-th job at day 1
+        return dp(0, 1)
+
+
+s = Solution()
+tests = [
+    (([6, 5, 4, 3, 2, 1], 2),
+     7),
+
+    (([9, 9, 9], 4),
+     -1),
+
+    (([1, 1, 1], 3),
+     3),
+]
+for inp, exp in tests:
+    res = s.minDifficulty(*inp)
+    if res != exp:
+        print("input: ", inp)
+        print("exp: ", exp)
+        print("got: ", res)
+        print()
+print(("All tests done"))
