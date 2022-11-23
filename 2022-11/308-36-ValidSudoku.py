17b41be64cd876d0aefe0ef31be2cccd59a2af4f
kftlfd <kftlfd@ex.ua>
2022-11-23 11:18:23 +0200

    day 308

---
+++ 2022-11/308-36-ValidSudoku.py
@@ -0,0 +1,126 @@
+"""
+Leetcode
+36. Valid Sudoku (medium)
+2022-11-23
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+ - Each row must contain the digits 1-9 without repetition.
+ - Each column must contain the digits 1-9 without repetition.
+ - Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+ - A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ - Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+"""
+
+from typing import List, Optional
+from collections import defaultdict
+
+
+# Runtime: 280 ms, faster than 9.72% of Python3 online submissions for Valid Sudoku.
+# Memory Usage: 13.9 MB, less than 83.37% of Python3 online submissions for Valid Sudoku.
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        def validateRows():
+            for row in board:
+                seen = set()
+                for col in row:
+                    if col == ".":
+                        continue
+                    if col in seen:
+                        return False
+                    seen.add(col)
+            return True
+
+        def validateCols():
+            n = 9
+            for col in range(n):
+                seen = set()
+                for row in range(n):
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+            return True
+
+        def validateSubBoxes():
+            d = [0, 3, 6]
+            for x in d:
+                for y in d:
+                    seen = set()
+                    for row in range(3):
+                        for col in range(3):
+                            r = row + x
+                            c = col + y
+                            if board[r][c] == ".":
+                                continue
+                            if board[r][c] in seen:
+                                return False
+                            seen.add(board[r][c])
+            return True
+
+        return all([validateRows(), validateCols(), validateSubBoxes()])
+
+
+# https://leetcode.com/problems/valid-sudoku/discuss/2840666/Python3-Easy-to-understand-beginner-friendly!
+# Runtime: 254 ms, faster than 23.75% of Python3 online submissions for Valid Sudoku.
+# Memory Usage: 13.9 MB, less than 36.06% of Python3 online submissions for Valid Sudoku.
+class Solution1:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+
+                cell = board[row][col]
+
+                if cell == ".":
+                    continue
+
+                if cell in rows[row]:
+                    return False
+                rows[row].add(cell)
+
+                if cell in cols[col]:
+                    return False
+                cols[col].add(cell)
+
+                boxId = ((row // 3) * 3) + (col // 3)
+                if cell in boxes[boxId]:
+                    return False
+                boxes[boxId].add(cell)
+
+        return True
