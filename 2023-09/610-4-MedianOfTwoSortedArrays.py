9f56771ae45203eda3c5008992044739c778515f
kftlfd <kftlfd@ex.ua>
2023-09-21 23:12:26 +0300

    day 610

---
+++ 2023-09/610-4-MedianOfTwoSortedArrays.py
@@ -0,0 +1,155 @@
+"""
+Leetcode
+4. Median of Two Sorted Arrays (hard)
+2023-09-21
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+Constraints:
+
+    nums1.length == m
+    nums2.length == n
+    0 <= m <= 1000
+    0 <= n <= 1000
+    1 <= m + n <= 2000
+    -10^6 <= nums1[i], nums2[i] <= 10^6
+"""
+
+from typing import List
+
+
+class Solution:
+    """
+    leetcode solution 1: Merge sort
+    Time: O(m + n)
+    Space: O(1)
+    Runtime: 90 ms, faster than 62.56% of Python3 online submissions for Median of Two Sorted Arrays.
+    Memory Usage: 16.6 MB, less than 68.59% of Python3 online submissions for Median of Two Sorted Arrays.
+    """
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        m, n = len(nums1), len(nums2)
+        p1, p2 = 0, 0
+
+        # Get the smaller value between nums1[p1] and nums2[p2].
+        def get_min():
+            nonlocal p1, p2
+            if p1 < m and p2 < n:
+                if nums1[p1] < nums2[p2]:
+                    ans = nums1[p1]
+                    p1 += 1
+                else:
+                    ans = nums2[p2]
+                    p2 += 1
+            elif p2 == n:
+                ans = nums1[p1]
+                p1 += 1
+            else:
+                ans = nums2[p2]
+                p2 += 1
+            return ans
+
+        if (m + n) % 2 == 0:
+            for _ in range((m + n) // 2 - 1):
+                _ = get_min()
+            return (get_min() + get_min()) / 2
+        else:
+            for _ in range((m + n) // 2):
+                _ = get_min()
+            return get_min()
+
+
+class Solution2:
+    """
+    leetcode solution 2: Binary search recursive
+    Time: O(log(m * n))
+    Space: O(log(m) + log(n))
+    Runtime: 90 ms, faster than 62.56% of Python3 online submissions for Median of Two Sorted Arrays.
+    Memory Usage: 17.5 MB, less than 9.45% of Python3 online submissions for Median of Two Sorted Arrays.
+    """
+
+    def findMedianSortedArrays(self, A: List[int], B: List[int]) -> float:
+        na, nb = len(A), len(B)
+        n = na + nb
+
+        def solve(k, a_start, a_end, b_start, b_end):
+            # If the segment of on array is empty, it means we have passed all
+            # its element, just return the corresponding element in the other array.
+            if a_start > a_end:
+                return B[k - a_start]
+            if b_start > b_end:
+                return A[k - b_start]
+
+            # Get the middle indexes and middle values of A and B.
+            a_index, b_index = (a_start + a_end) // 2, (b_start + b_end) // 2
+            a_value, b_value = A[a_index], B[b_index]
+
+            # If k is in the right half of A + B, remove the larger right half.
+            if a_index + b_index < k:
+                if a_value > b_value:
+                    return solve(k, a_start, a_end, b_index + 1, b_end)
+                else:
+                    return solve(k, a_index + 1, a_end, b_start, b_end)
+            # Otherwise, remove the smaller left half.
+            else:
+                if a_value > b_value:
+                    return solve(k, a_start, a_index - 1, b_start, b_end)
+                else:
+                    return solve(k, a_start, a_end, b_start, b_index - 1)
+
+        if n % 2:
+            return solve(n // 2, 0, na - 1, 0, nb - 1)
+        else:
+            return (solve(n // 2 - 1, 0, na - 1, 0, nb - 1) + solve(n // 2, 0, na - 1, 0, nb - 1)) / 2
+
+
+class Solution3:
+    """
+    leetcode solution 3: Better binary search
+    Time: O(log(min(m, n)))
+    Space: O(1)
+    Runtime: 86 ms, faster than 79.70% of Python3 online submissions for Median of Two Sorted Arrays.
+    Memory Usage: 16.5 MB, less than 91.11% of Python3 online submissions for Median of Two Sorted Arrays.
+    """
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right:
+            partitionA = (left + right) // 2
+            partitionB = (m + n + 1) // 2 - partitionA
+
+            maxLeftA = float(
+                '-inf') if partitionA == 0 else nums1[partitionA - 1]
+            minRightA = float('inf') if partitionA == m else nums1[partitionA]
+            maxLeftB = float(
+                '-inf') if partitionB == 0 else nums2[partitionB - 1]
+            minRightB = float('inf') if partitionB == n else nums2[partitionB]
+
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
+                else:
+                    return max(maxLeftA, maxLeftB)
+            elif maxLeftA > minRightB:
+                right = partitionA - 1
+            else:
+                left = partitionA + 1
