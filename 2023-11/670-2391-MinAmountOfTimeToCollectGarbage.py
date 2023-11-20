553ee9689ac257fc9132d97b71ac56ef1875e83b
kftlfd <kftlfd@ex.ua>
2023-11-20 23:48:23 +0200

    day 670

---
+++ 2023-11/670-2391-MinAmountOfTimeToCollectGarbage.py
@@ -0,0 +1,121 @@
+"""
+Leetcode
+2391. Minimum Amount of Time to Collect Garbage (medium)
+2023-11-20
+
+You are given a 0-indexed array of strings garbage where garbage[i] represents the assortment of garbage at the ith house. garbage[i] consists only of the characters 'M', 'P' and 'G' representing one unit of metal, paper and glass garbage respectively. Picking up one unit of any type of garbage takes 1 minute.
+
+You are also given a 0-indexed integer array travel where travel[i] is the number of minutes needed to go from house i to house i + 1.
+
+There are three garbage trucks in the city, each responsible for picking up one type of garbage. Each garbage truck starts at house 0 and must visit each house in order; however, they do not need to visit every house.
+
+Only one garbage truck may be used at any given moment. While one truck is driving or picking up garbage, the other two trucks cannot do anything.
+
+Return the minimum number of minutes needed to pick up all the garbage.
+
+ 
+
+Example 1:
+
+Input: garbage = ["G","P","GP","GG"], travel = [2,4,3]
+Output: 21
+Explanation:
+The paper garbage truck:
+1. Travels from house 0 to house 1
+2. Collects the paper garbage at house 1
+3. Travels from house 1 to house 2
+4. Collects the paper garbage at house 2
+Altogether, it takes 8 minutes to pick up all the paper garbage.
+The glass garbage truck:
+1. Collects the glass garbage at house 0
+2. Travels from house 0 to house 1
+3. Travels from house 1 to house 2
+4. Collects the glass garbage at house 2
+5. Travels from house 2 to house 3
+6. Collects the glass garbage at house 3
+Altogether, it takes 13 minutes to pick up all the glass garbage.
+Since there is no metal garbage, we do not need to consider the metal garbage truck.
+Therefore, it takes a total of 8 + 13 = 21 minutes to collect all the garbage.
+
+Example 2:
+
+Input: garbage = ["MMM","PGM","GP"], travel = [3,10]
+Output: 37
+Explanation:
+The metal garbage truck takes 7 minutes to pick up all the metal garbage.
+The paper garbage truck takes 15 minutes to pick up all the paper garbage.
+The glass garbage truck takes 15 minutes to pick up all the glass garbage.
+It takes a total of 7 + 15 + 15 = 37 minutes to collect all the garbage.
+
+ 
+
+Constraints:
+
+    2 <= garbage.length <= 10^5
+    garbage[i] consists of only the letters 'M', 'P', and 'G'.
+    1 <= garbage[i].length <= 10
+    travel.length == garbage.length - 1
+    1 <= travel[i] <= 100
+"""
+
+from typing import List
+
+
+class Solution:
+    """
+    Runtime: 1077 ms, faster than 16.19% of Python3 online submissions for Minimum Amount of Time to Collect Garbage.
+    Memory Usage: 39.3 MB, less than 61.82% of Python3 online submissions for Minimum Amount of Time to Collect Garbage.
+    """
+
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        ans = 0
+
+        travel = [0] + travel
+
+        started = [False, False, False]
+
+        def parse(s: str) -> tuple[int, int, int]:
+            out = [0, 0, 0]
+
+            for c in s:
+                if c == "M":
+                    out[0] += 1
+                elif c == "P":
+                    out[1] += 1
+                else:  # c == "G"
+                    out[2] += 1
+
+            return out
+
+        for i in range(len(garbage) - 1, -1, -1):
+            house = parse(garbage[i])
+
+            for garb_type in range(3):
+                house_garb = house[garb_type]
+                ans += house_garb
+                if house_garb > 0 and not started[garb_type]:
+                    started[garb_type] = True
+                if started[garb_type]:
+                    ans += travel[i]
+
+        return ans
+
+
+class Solution1:
+    """
+    https://leetcode.com/problems/minimum-amount-of-time-to-collect-garbage/solution/2141614
+    Runtime: 802 ms, faster than 90.41% of Python3 online submissions for Minimum Amount of Time to Collect Garbage.
+    Memory Usage: 39.4 MB, less than 34.21% of Python3 online submissions for Minimum Amount of Time to Collect Garbage.
+    """
+
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        G = P = M = False
+        ans = len(garbage[0])
+
+        for i in range(len(travel), 0, -1):
+            G |= 'G' in garbage[i]
+            P |= 'P' in garbage[i]
+            M |= 'M' in garbage[i]
+            ans += travel[i-1] * (G + P + M) + len(garbage[i])
+
+        return ans
