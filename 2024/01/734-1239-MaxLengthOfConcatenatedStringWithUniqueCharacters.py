cff61982254e5a8cefffbc5805d8cf6ca2b8e327
kftlfd <kftlfd@ex.ua>
2024-01-23 14:34:35 +0200

    day 734

---
+++ 2024/01/734-1239-MaxLengthOfConcatenatedStringWithUniqueCharacters.py
@@ -0,0 +1,74 @@
+"""
+Leetcode
+1239. Maximum Length of a Concatenated String with Unique Characters
+Medium
+2024-01-23
+
+You are given an array of strings arr. A string s is formed by the concatenation of a subsequence of arr that has unique characters.
+
+Return the maximum possible length of s.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: arr = ["un","iq","ue"]
+Output: 4
+Explanation: All the valid concatenations are:
+- ""
+- "un"
+- "iq"
+- "ue"
+- "uniq" ("un" + "iq")
+- "ique" ("iq" + "ue")
+Maximum length is 4.
+
+Example 2:
+
+Input: arr = ["cha","r","act","ers"]
+Output: 6
+Explanation: Possible longest valid concatenations are "chaers" ("cha" + "ers") and "acters" ("act" + "ers").
+
+Example 3:
+
+Input: arr = ["abcdefghijklmnopqrstuvwxyz"]
+Output: 26
+Explanation: The only string in arr has all 26 characters.
+
+ 
+
+Constraints:
+
+    1 <= arr.length <= 16
+    1 <= arr[i].length <= 26
+    arr[i] contains only lowercase English letters.
+"""
+
+from typing import List
+
+
+class Solution:
+    """
+    Runtime: 1296 ms, faster than 5.01% of Python3 online submissions for Maximum Length of a Concatenated String with Unique Characters.
+    Memory Usage: 16.6 MB, less than 78.18% of Python3 online submissions for Maximum Length of a Concatenated String with Unique Characters.
+    """
+
+    def maxLength(self, arr: List[str]) -> int:
+        ans = 0
+
+        def dfs(idx, picked):
+            nonlocal ans
+
+            if idx >= len(arr):
+                word = "".join(arr[i] for i in picked)
+                if len(word) == len(set(word)):
+                    ans = max(ans, len(word))
+                return
+
+            dfs(idx + 1, picked)
+            dfs(idx + 1, picked + [idx])
+
+        dfs(0, [])
+        return ans
