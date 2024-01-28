7a7c8ddfe9ec6d5a86298a1126d8dd6192c9b92e
kftlfd <kftlfd@ex.ua>
2024-01-28 13:25:38 +0200

    day 739

---
+++ 2024/01/739-1074-NumberOfSubmatricesThatSumToTarget.py
@@ -0,0 +1,70 @@
+"""
+Leetcode
+1074. Number of Submatrices That Sum to Target
+Hard
+2024-01-28
+
+Given a matrix and a target, return the number of non-empty submatrices that sum to target.
+
+A submatrix x1, y1, x2, y2 is the set of all cells matrix[x][y] with x1 <= x <= x2 and y1 <= y <= y2.
+
+Two submatrices (x1, y1, x2, y2) and (x1', y1', x2', y2') are different if they have some coordinate that is different: for example, if x1 != x1'.
+
+ 
+
+Example 1:
+
+Input: matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0
+Output: 4
+Explanation: The four 1x1 submatrices that only contain 0.
+
+Example 2:
+
+Input: matrix = [[1,-1],[-1,1]], target = 0
+Output: 5
+Explanation: The two 1x2 submatrices, plus the two 2x1 submatrices, plus the 2x2 submatrix.
+
+Example 3:
+
+Input: matrix = [[904]], target = 0
+Output: 0
+
+ 
+
+Constraints:
+
+    1 <= matrix.length <= 100
+    1 <= matrix[0].length <= 100
+    -1000 <= matrix[i] <= 1000
+    -10^8 <= target <= 10^8
+
+Hints:
+- Using a 2D prefix sum, we can query the sum of any submatrix in O(1) time. Now for each (r1, r2), we can find the largest sum of a submatrix that uses every row in [r1, r2] in linear time using a sliding window.
+"""
+
+from collections import Counter
+from itertools import accumulate, combinations
+from typing import List
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/discuss/1162611/Python-cumulative-sum-+-dp-explained
+    """
+
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        dp, ans = {}, 0
+
+        for k in range(m):
+            t = [0] + list(accumulate(matrix[k]))
+            for i, j in combinations(range(n+1), 2):
+                dp[i, j, k] = dp.get((i, j, k-1), 0) + t[j] - t[i]
+
+        for i, j in combinations(range(n+1), 2):
+            T = Counter([0])
+            for k in range(m):
+                ans += T[dp[i, j, k] - target]
+                T[dp[i, j, k]] += 1
+
+        return ans
