a5a47962da16077faf397329d7e9b4e45cda1569
kftlfd <kftlfd@ex.ua>
2024-04-05 12:56:18 +0300

    day 807

---
+++ 2024/04/807-1544-MakeTheStringGreat.py
@@ -0,0 +1,77 @@
+"""
+Leetcode
+1544. Make The String Great
+Easy
+2024-04-05
+
+Given a string s of lower and upper case English letters.
+
+A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+    0 <= i <= s.length - 2
+    s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+
+To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+
+Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+Notice that an empty string is also good.
+
+ 
+
+Example 1:
+
+Input: s = "leEeetcode"
+Output: "leetcode"
+Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+
+Example 2:
+
+Input: s = "abBAcC"
+Output: ""
+Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+"abBAcC" --> "aAcC" --> "cC" --> ""
+"abBAcC" --> "abBA" --> "aA" --> ""
+
+Example 3:
+
+Input: s = "s"
+Output: "s"
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 100
+    s contains only lower and upper case English letters.
+"""
+
+
+class Solution:
+    """
+    Runtime: 48 ms, faster than 5.96% of Python3 online submissions for Make The String Great.
+    Memory Usage: 16.5 MB, less than 91.04% of Python3 online submissions for Make The String Great.
+    """
+
+    def makeGood(self, s: str) -> str:
+        chars = list(s)
+
+        def removeBad() -> bool:
+            changes = False
+
+            i = len(chars) - 2
+            while i >= 0:
+                if abs(ord(chars[i]) - ord(chars[i + 1])) == 32:
+                    del chars[i+1]
+                    del chars[i]
+                    i -= 2
+                    changes = True
+                else:
+                    i -= 1
+
+            return changes
+
+        while removeBad():
+            pass
+
+        return "".join(chars)
