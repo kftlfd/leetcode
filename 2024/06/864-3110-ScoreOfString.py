da81d1afde5798c72b59c5713d8021940eb21ea7
kftlfd <kftlfd@ex.ua>
2024-06-01 10:43:47 +0300

    day 864

---
+++ 2024/06/864-3110-ScoreOfString.py
@@ -0,0 +1,49 @@
+"""
+Leetcode
+3110. Score of a String
+Easy
+2024-06-01
+
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+
+ 
+
+Example 1:
+
+Input: s = "hello"
+
+Output: 13
+
+Explanation:
+
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+
+Input: s = "zaz"
+
+Output: 50
+
+Explanation:
+
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+ 
+
+Constraints:
+
+    2 <= s.length <= 100
+    s consists only of lowercase English letters.
+"""
+
+
+class Solution:
+    """
+    Runtime: 41 ms, faster than 24.95% of Python3 online submissions for Score of a String.
+    Memory Usage: 16.3 MB, less than 98.12% of Python3 online submissions for Score of a String.
+    """
+
+    def scoreOfString(self, s: str) -> int:
+        return sum(abs(ord(a) - ord(b)) for a, b in zip(s[:-1], s[1:]))
