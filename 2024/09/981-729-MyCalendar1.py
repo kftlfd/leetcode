f360280531cee3af55dde036afe91cbbf245ca98
kftlfd <kftlfd@ex.ua>
2024-09-26 11:44:03 +0300

    day 981

---
+++ 2024/09/981-729-MyCalendar1.py
@@ -0,0 +1,66 @@
+"""
+Leetcode
+2024-09-26
+729. My Calendar I
+Medium
+
+You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+Implement the MyCalendar class:
+
+    MyCalendar() Initializes the calendar object.
+    boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+
+ 
+
+Example 1:
+
+Input
+["MyCalendar", "book", "book", "book"]
+[[], [10, 20], [15, 25], [20, 30]]
+Output
+[null, true, false, true]
+
+Explanation
+MyCalendar myCalendar = new MyCalendar();
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+
+ 
+
+Constraints:
+
+    0 <= start < end <= 10^9
+    At most 1000 calls will be made to book.
+"""
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
+
+
+from bisect import bisect_right
+
+
+class MyCalendar:
+    """
+    Runtime: 154 ms, faster than 92.53% of Python3 online submissions for My Calendar I.
+    Memory Usage: 17.3 MB, less than 89.44% of Python3 online submissions for My Calendar I.
+    """
+
+    def __init__(self):
+        inf = float('inf')
+        self.events = [[-inf, -inf], [inf, inf]]
+
+    def book(self, start: int, end: int) -> bool:
+        idx = bisect_right(self.events, start, key=lambda x: x[0])
+        if self.events[idx - 1][1] > start or self.events[idx][0] < end:
+            return False
+        self.events.insert(idx, [start, end])
+        return True
