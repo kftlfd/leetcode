fdf2b32c49e5cf4387335c3f9548c789afeb97c6
kftlfd <kftlfd@ex.ua>
2025-05-08 11:54:28 +0300

    day 1205

---
+++ 2025/05/1205-3342-FindMinTimeToReachLastRoom2.py
@@ -0,0 +1,147 @@
+"""
+Leetcode
+2025-05-08
+3342. Find Minimum Time to Reach Last Room II
+Medium
+
+There is a dungeon with n x m rooms arranged as a grid.
+
+You are given a 2D array moveTime of size n x m, where moveTime[i][j] represents the minimum time in seconds when you can start moving to that room. You start from the room (0, 0) at time t = 0 and can move to an adjacent room. Moving between adjacent rooms takes one second for one move and two seconds for the next, alternating between the two.
+
+Return the minimum time to reach the room (n - 1, m - 1).
+
+Two rooms are adjacent if they share a common wall, either horizontally or vertically.
+
+ 
+
+Example 1:
+
+Input: moveTime = [[0,4],[4,4]]
+
+Output: 7
+
+Explanation:
+
+The minimum time required is 7 seconds.
+
+    At time t == 4, move from room (0, 0) to room (1, 0) in one second.
+    At time t == 5, move from room (1, 0) to room (1, 1) in two seconds.
+
+Example 2:
+
+Input: moveTime = [[0,0,0,0],[0,0,0,0]]
+
+Output: 6
+
+Explanation:
+
+The minimum time required is 6 seconds.
+
+    At time t == 0, move from room (0, 0) to room (1, 0) in one second.
+    At time t == 1, move from room (1, 0) to room (1, 1) in two seconds.
+    At time t == 3, move from room (1, 1) to room (1, 2) in one second.
+    At time t == 4, move from room (1, 2) to room (1, 3) in two seconds.
+
+Example 3:
+
+Input: moveTime = [[0,1],[1,2]]
+
+Output: 4
+
+ 
+
+Constraints:
+
+    2 <= n == moveTime.length <= 750
+    2 <= m == moveTime[i].length <= 750
+    0 <= moveTime[i][j] <= 10^9
+
+
+Hint 1
+Use shortest path algorithms with a state for the last move being odd or even indexed.
+"""
+
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    """
+    Runtime 1854ms Beats 60.94%
+    Memory 90.65MB Beats 71.67%
+    """
+
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        m = len(moveTime)
+        n = len(moveTime[0])
+
+        time = [[float('inf')] * n for _ in range(m)]
+        time[0][0] = 0
+        q = [(0, 0, 0, 0)]
+
+        dirs = ((-1, 0), (0, -1), (1, 0), (0, 1))
+
+        while q:
+            t, r, c, move = heappop(q)
+
+            if r == m - 1 and c == n - 1:
+                return t
+
+            for nr, nc in ((r + dr, c + dc) for dr, dc in dirs):
+                if not 0 <= nr < m or not 0 <= nc < n:
+                    continue
+                nt = max(t, moveTime[nr][nc]) + 1 + move
+                if nt >= time[nr][nc]:
+                    continue
+                time[nr][nc] = nt
+                heappush(q, (nt, nr, nc, move ^ 1))
+
+        return time[-1][-1]
+
+
+class Solution1:
+    """
+    leetcode solution: Shortest Path + Dijkstra
+    Runtime 2762ms Beats 32.62%
+    Memory 93.23MB Beats 55.36%
+    """
+
+    class State:
+        def __init__(self, x, y, dis):
+            self.x = x
+            self.y = y
+            self.dis = dis
+
+        def __lt__(self, other):
+            return self.dis < other.dis
+
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n = len(moveTime)
+        m = len(moveTime[0])
+        inf = float("inf")
+        d = [[inf] * m for _ in range(n)]
+        v = [[0] * m for _ in range(n)]
+
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        d[0][0] = 0
+        q = []
+        heappush(q, self.State(0, 0, 0))
+
+        while q:
+            s = heappop(q)
+            if v[s.x][s.y]:
+                continue
+            if s.x == n - 1 and s.y == m - 1:
+                break
+            v[s.x][s.y] = 1
+            for dx, dy in dirs:
+                nx, ny = s.x + dx, s.y + dy
+                if not (0 <= nx < n and 0 <= ny < m):
+                    continue
+                dist = max(d[s.x][s.y], moveTime[nx][ny]) + (s.x + s.y) % 2 + 1
+                if d[nx][ny] > dist:
+                    d[nx][ny] = dist
+                    heappush(q, self.State(nx, ny, dist))
+
+        return d[n - 1][m - 1]
