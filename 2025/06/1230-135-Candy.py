5de99294fa976979f853a704c58a12f44136e1c7
kftlfd <kftlfd@ex.ua>
2025-06-02 13:34:37 +0300

    day 1230

---
+++ 2025/06/1230-135-Candy.py
@@ -0,0 +1,75 @@
+"""
+Leetcode
+2025-06-02
+135. Candy
+Hard
+
+There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+
+You are giving candies to these children subjected to the following requirements:
+
+    Each child must have at least one candy.
+    Children with a higher rating get more candies than their neighbors.
+
+Return the minimum number of candies you need to have to distribute the candies to the children.
+
+ 
+
+Example 1:
+
+Input: ratings = [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+
+Example 2:
+
+Input: ratings = [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+The third child gets 1 candy because it satisfies the above two conditions.
+
+ 
+
+Constraints:
+
+    n == ratings.length
+    1 <= n <= 2 * 10^4
+    0 <= ratings[i] <= 2 * 10^4
+
+
+"""
+
+from typing import List
+
+
+class Solution:
+    """
+    Runtime 16ms Beats 53.76%
+    Memory 19.91MB Beats 36.16%
+    """
+
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        candies = [1] * n
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        return sum(candies)
+
+
+s = Solution()
+tests = [
+    ([1, 3, 4, 5, 2],
+     11),
+]
+for inp, exp in tests:
+    res = s.candy(inp)
+    if res != exp:
+        print('input:  ', inp)
+        print('expect: ', exp)
+        print('output: ', res)
+        print()
+print('Completed testing')
