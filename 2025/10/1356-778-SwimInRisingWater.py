59e918402cbeae97fa72323630362d3ea454898f
kftlfd <kftlfd@ex.ua>
2025-10-06 12:02:48 +0300

    day 1356

---
+++ 2025/10/1356-778-SwimInRisingWater.py
@@ -0,0 +1,79 @@
+"""
+Leetcode
+2025-10-06
+778. Swim in Rising Water
+Hard
+
+You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+
+It starts raining, and water gradually rises over time. At time t, the water level is t, meaning any cell with elevation less than equal to t is submerged or reachable.
+
+You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+Return the minimum time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0).
+
+ 
+
+Example 1:
+
+Input: grid = [[0,2],[1,3]]
+Output: 3
+Explanation:
+At time 0, you are in grid location (0, 0).
+You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+You cannot reach point (1, 1) until time 3.
+When the depth of water is 3, we can swim anywhere inside the grid.
+
+Example 2:
+
+Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+Output: 16
+Explanation: The final route is shown.
+We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+
+ 
+
+Constraints:
+
+    n == grid.length
+    n == grid[i].length
+    1 <= n <= 50
+    0 <= grid[i][j] < n2
+    Each value grid[i][j] is unique.
+
+
+Hint 1
+Use either Dijkstra's, or binary search for the best time T for which you can reach the end if you only step on squares at most T.
+"""
+
+from heapq import heappop, heappush
+from typing import List
+
+
+class Solution:
+    """
+    Runtime 26ms Beats 68.87%
+    Memory 18.18MB Beats 90.82%
+    """
+
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        inf = n*n
+        time = [[inf] * n for _ in range(n)]
+        time[0][0] = grid[0][0]
+        q = [(grid[0][0], 0, 0)]
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        while q:
+            t, r, c = heappop(q)
+
+            if r == n - 1 and c == n - 1:
+                return t
+
+            for nr, nc in ((r + dr, c + dc) for dr, dc in dirs):
+                if 0 <= nr < n and 0 <= nc < n and time[nr][nc] == inf:
+                    nt = max(t, grid[nr][nc])
+                    time[nr][nc] = nt
+                    heappush(q, (nt, nr, nc))
+
+        return time[n-1][n-1]
