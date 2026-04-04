faf2dbb5448c39f108f2763a49d0b8b0e3e0561b
kftlfd <kftlfd@ex.ua>
2026-04-04 12:26:36 +0300

    day 1536

---
+++ 2026/04/1536-2075-DecodeTheSlantedCiphertext.py
@@ -0,0 +1,107 @@
+"""
+Leetcode
+2026-04-04
+2075. Decode the Slanted Ciphertext
+Medium
+
+A string originalText is encoded using a slanted transposition cipher to a string encodedText with the help of a matrix having a fixed number of rows rows.
+
+originalText is placed first in a top-left to bottom-right manner.
+
+The blue cells are filled first, followed by the red cells, then the yellow cells, and so on, until we reach the end of originalText. The arrow indicates the order in which the cells are filled. All empty cells are filled with ' '. The number of columns is chosen such that the rightmost column will not be empty after filling in originalText.
+
+encodedText is then formed by appending all characters of the matrix in a row-wise fashion.
+
+The characters in the blue cells are appended first to encodedText, then the red cells, and so on, and finally the yellow cells. The arrow indicates the order in which the cells are accessed.
+
+For example, if originalText = "cipher" and rows = 3, then we encode it in the following manner:
+
+The blue arrows depict how originalText is placed in the matrix, and the red arrows denote the order in which encodedText is formed. In the above example, encodedText = "ch ie pr".
+
+Given the encoded string encodedText and number of rows rows, return the original string originalText.
+
+Note: originalText does not have any trailing spaces ' '. The test cases are generated such that there is only one possible originalText.
+
+ 
+
+Example 1:
+
+Input: encodedText = "ch   ie   pr", rows = 3
+Output: "cipher"
+Explanation: This is the same example described in the problem description.
+
+Example 2:
+
+Input: encodedText = "iveo    eed   l te   olc", rows = 4
+Output: "i love leetcode"
+Explanation: The figure above denotes the matrix that was used to encode originalText. 
+The blue arrows show how we can find originalText from encodedText.
+
+Example 3:
+
+Input: encodedText = "coding", rows = 1
+Output: "coding"
+Explanation: Since there is only 1 row, both originalText and encodedText are the same.
+
+ 
+
+Constraints:
+
+    0 <= encodedText.length <= 10^6
+    encodedText consists of lowercase English letters and ' ' only.
+    encodedText is a valid encoding of some originalText that does not have trailing spaces.
+    1 <= rows <= 1000
+    The testcases are generated such that there is only one possible originalText.
+
+
+"""
+
+
+class Solution:
+    """
+    Runtime 95ms Beats 75.97%
+    Memory 34.41MB Beats 20.78%
+    """
+
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        if rows == 1:
+            return encodedText
+
+        row_len = len(encodedText) // rows
+        grid = [list(encodedText[i*row_len:(i+1)*row_len])
+                for i in range(rows)]
+        ans = []
+        for i in range(row_len):
+            r, c = 0, i
+            while r < rows and c < row_len:
+                ans.append(grid[r][c])
+                r += 1
+                c += 1
+        return "".join(ans).rstrip()
+
+
+class Solution1:
+    """
+    sample 2ms solution
+    Runtime 3ms Beats 96.75%
+    Memory 26.42MB Beats 95.45%
+    """
+
+    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        if rows == 1:
+            return encodedText
+
+        N = len(encodedText)
+        cols = N // rows
+        i, j, k = 0, 0, 0
+        original_text = []
+
+        while k < N:
+            original_text.append(encodedText[k])
+            i += 1
+            if i == rows:
+                i = 0
+                j += 1
+            k = i*(cols + 1) + j
+
+        return ''.join(original_text).rstrip()
