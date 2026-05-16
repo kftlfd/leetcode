3d0aa5c121e8bac11543800543ee3943b9c4d206
kftlfd <kftlfd@ex.ua>
2026-05-16 13:40:31 +0300

    day 1578

---
+++ 2026/05/1578-154-FindMinimumInRotatedSortedArray2.py
@@ -0,0 +1,77 @@
+"""
+Leetcode
+2026-05-16
+154. Find Minimum in Rotated Sorted Array II
+Hard
+
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+
+    [4,5,6,7,0,1,4] if it was rotated 4 times.
+    [0,1,4,4,5,6,7] if it was rotated 7 times.
+
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+
+You must decrease the overall operation steps as much as possible.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5]
+Output: 1
+
+Example 2:
+
+Input: nums = [2,2,2,0,1]
+Output: 0
+
+ 
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 5000
+    -5000 <= nums[i] <= 5000
+    nums is sorted and rotated between 1 and n times.
+
+ 
+
+Follow up: This problem is similar to Find Minimum in Rotated Sorted Array, but nums may contain duplicates. Would this affect the runtime complexity? How and why?
+"""
+
+from typing import List
+
+
+class Solution:
+    """
+    Runtime 0ms Beats 100.00%
+    Memory 19.51MB Beats 55.91%
+    """
+
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
+
+
+class Solution1:
+    """
+    sample solution
+    Runtime 0ms Beats 100.00%
+    Memory 19.47MB Beats 90.27%
+    """
+
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] > nums[r]:
+                l = m + 1
+            elif nums[m] < nums[r]:
+                r = m
+            else:
+                r = r - 1
+
+        return nums[l]
