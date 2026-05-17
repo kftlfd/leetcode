bdb88eccb0c1afab2e896424acb84b444aaefa96
kftlfd <kftlfd@ex.ua>
2026-05-17 13:37:57 +0300

    day 1579

---
+++ 2026/05/1579-1306-JumpGame3.py
@@ -0,0 +1,82 @@
+"""
+Leetcode
+2026-05-17
+1306. Jump Game III
+Medium
+
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+
+Example 2:
+
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+
+Example 3:
+
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+
+ 
+
+Constraints:
+
+    1 <= arr.length <= 5 * 10^4
+    0 <= arr[i] < arr.length
+    0 <= start < arr.length
+
+
+"""
+
+from collections import deque
+from typing import List
+
+
+class Solution:
+    """
+    Runtime 7ms Beats 97.28%
+    Memory 26.64MB Beats 47.47%
+    """
+
+    def canReach(self, arr: List[int], start: int) -> bool:
+        n = len(arr)
+
+        q = deque([start])
+        seen = {start}
+
+        while q:
+            cur = q.popleft()
+
+            left = cur - arr[cur]
+            if left >= 0:
+                if arr[left] == 0:
+                    return True
+                if left not in seen:
+                    seen.add(left)
+                    q.append(left)
+
+            right = cur + arr[cur]
+            if right < n:
+                if arr[right] == 0:
+                    return True
+                if right not in seen:
+                    seen.add(right)
+                    q.append(right)
+
+        return False
